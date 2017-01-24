fff6b8d4f668890ff018aebf199b20b28a9995ce
Evgeny Shpilevsky <ftdebugger@yandex-team.ru>
2017-01-24 12:41:09 +0300

    Add ES6 module support

--- src/index.js
+++ src/index.js
@@ -9,8 +9,19 @@ export default function ({types, template}) {
                         CLASS_NAME: types.identifier(path.node.id.name),
                         DISPLAY_NAME: types.stringLiteral(path.node.id.name)
                     });
-                    
-                    path.insertAfter(ast);
+
+                    let parent = path.parentPath,
+                        prev = path;
+
+                    while(!parent || (parent.node.type !== 'Program' && parent.node.type !== 'BlockStatement')) {
+                        prev = parent;
+                        parent = parent.parentPath;
+                    }
+
+                    if (parent) {
+                        prev.insertAfter(ast);
+                        // parent.pushContainer('body', ast);
+                    }
                 }
             }
         }
